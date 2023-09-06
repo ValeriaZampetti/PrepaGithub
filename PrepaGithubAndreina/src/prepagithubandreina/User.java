a4f5145f9b5caddc916105933c96052be2ead504
andreinarivas <113389360+andreinarivas@users.noreply.github.com>
2023-09-06 17:13:20 -0400

    cambio en nodo y clase user

---
+++ PrepaGithubAndreina/src/prepagithubandreina/User.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prepagithubandreina;
+
+/**
+ *
+ * @author andre
+ */
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    
+    
+}
